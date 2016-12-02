83e1ba0215e6c0a85f62b99a5afd624944df31b7
yudaitateishi <b133736@hiroshima-u.ac.jp>
2016-12-02 11:47:04 +0900

    add binom.test p-value output in task4.py

--- search_mutation/task4.py
+++ search_mutation/task4.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+import pandas as pd
+import pyper
+r = pyper.R(use_pandas = "True")
 
 if len(sys.argv) < 4:
 	print("Usage: #python task4.py mutation_list mutation_count output_file")
@@ -21,24 +24,32 @@ with open(sys.argv[2],'r') as count_file:
 truncating_type = ['Frame_Shift_Ins','Frame_Shift_Del','Nonsense_Mutation','Splice_Site']
 truncating_count = {}
 for mutation in mutation_list:#make dict {key=gene name:value=truncating count,...}
-	if mutation[0] in truncating_count:#mutation[0] = gene name
-		if mutation[1] in truncating_type:#mutation[1] = mutation type
-			truncating_count[mutation[0]] += 1.0
+	if mutation[1] in truncating_count:#mutation[0] = gene name
+		if mutation[2] in truncating_type:#mutation[1] = mutation type
+			truncating_count[mutation[1]] += 1.0
 	else:
-		if mutation[1] in truncating_type:
-			truncating_count[mutation[0]] = 1.0
+		if mutation[2] in truncating_type:
+			truncating_count[mutation[1]] = 1.0
 
 #take average
 truncating_avg = {}
+p_value = {}
 for gene,count in mutation_count.items():
 	if gene in truncating_count:
 		truncating_avg[gene] = round(truncating_count[gene] / count,4)
 	else:
 		truncating_avg[gene] = 0
+		truncating_count[gene] = 0
+	r.assign("T",count)
+	r.assign("S",truncating_count[gene])
+	r("R<-binom.test(S,T,0.5,alternative=\"greater\")")
+	p_value[gene] = r.get("R$p.value")
 
-with open(sys.argv[3],'w') as result: 
-	for k,v in sorted(truncating_avg.items(),key=lambda x:float((x[1]+1)*mutation_count[x[0]]),reverse=True):
-		result.write(k + '\t'+ str(mutation_count[k]) + '\t' + str(v) + '\n')
+
+with open(sys.argv[3],'w') as result:
+	result.write("gene\tmutation\ttruncating\taverage\tp-value\n") 
+	for k,v in sorted(p_value.items(),key=lambda x:x[1]):
+		result.write(k + '\t'+ str(mutation_count[k]) + '\t' + str(truncating_count[k]) + "\t" + str(truncating_avg[k])+ "\t" + str(v) + '\n')
 	print("output truncating average")
 
 
